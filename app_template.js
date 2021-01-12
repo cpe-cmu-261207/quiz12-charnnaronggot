5902af870bfb705368744cf4098bda24e3b8e317
charnnaronggot <59276771+charnnaronggot@users.noreply.github.com>
2021-01-12 13:58:56 +0700

    Update app_template.js

--- app_template.js
+++ app_template.js
@@ -1,1 +1,11 @@
 //your code here!
+//your code here!
+var num = document.getElementById('input-number');
+let massage = document.getElementById('message-1');
+
+
+const StuNum = 620612146 ;
+
+massage.innerHTML = StuNum + num ;
+
+ 
